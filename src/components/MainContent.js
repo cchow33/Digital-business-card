022afa05ac4593a10d1adfa31ed4619ad89331bb
cchow33 <109078860+cchow33@users.noreply.github.com>
2022-10-10 23:08:42 -0400

    Create MainContent.js

---
+++ src/components/MainContent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function MainContent () {
+  return (
+
+    <section className="info-container">
+      <h2>About</h2>
+      <p>As an eternal optimist and over-achiever, I serve as the Deputy Director of the Parks and Recreation Department of Pawnee, Indiana. </p>
+        
+      <h2>Interests</h2>
+      <p>Waffles and strong women in power like Hillary Clinton</p>
+    </section> 
+        
+  )
+}
